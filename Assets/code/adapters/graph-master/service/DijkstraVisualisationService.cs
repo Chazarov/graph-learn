a06331b81cb7f7c2f9966a3ad2b784995fe16bac
Chazarow <zaharowanrew@gmail.com>
2025-12-20 18:30:18 +0400

    feat: update project settings and buid

--- Assets/code/adapters/graph-master/service/DijkstraVisualisationService.cs
+++ Assets/code/adapters/graph-master/service/DijkstraVisualisationService.cs
@@ -1,9 +1,6 @@
 using Domain;
 using GraphMaster.Visualization.Actions;
 using System.Collections.Generic;
-using System.Linq;
-using UnityEditor.Experimental.GraphView;
-using UnityEngine;
 
 namespace GraphMaster
 {
